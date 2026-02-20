10ac7aa2752b02982c41a26568580a4fad8deb55
tve24el030-svg <tve24el030@cet.ac.in>
2026-02-20 09:34:11 +0530

    code uploaded

--- src/CustomException.java
+++ src/CustomException.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
-// TODO: Create class InvalidAgeException extends Exception
-// Create a constructor that accepts a message string and passes it to super(message)
+class InvalidAgeException extends Exception{
+	public InvalidAgeException(String message){
+		super(message);
+	}
+}
 
 public class CustomException {
-    
-    // TODO: Create a static method validate(int age) that throws InvalidAgeException
-    // Check if age < 18, throw new InvalidAgeException("Not eligible to vote")
-    // Else print "Eligible to vote"
 
-    
-        // TODO: Read age
-        
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int age=sc.nextInt();
+		try {
+            		if (age < 18) {
+                		throw new InvalidAgeException("Not eligible to vote");
+            		} else {
+                		System.out.println("Eligible to vote");
+            		}
+        	} catch (InvalidAgeException e) {
+            		System.out.println("Exception: " + e.getMessage());
+        	}
+	}
 }
